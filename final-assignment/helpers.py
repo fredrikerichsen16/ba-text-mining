4830d9547490507c15007127bdf0a53edf059efc
fredrikerichsen16 <34533870+fredrikerichsen16@users.noreply.github.com>
2022-04-08 21:38:31 +0200

    Did some analysis on reddit comments + Finalized algorithm

--- final-assignment/helpers.py
+++ final-assignment/helpers.py
@@ -6,12 +6,37 @@ def remove_urls(txt):
 def replace_usernames(txt):
     return re.sub(r'(^|\s)@(\w+)', ' @user', txt, flags=re.MULTILINE)
 
+def remove_trailing_and_leading_hashtags(tweet):
+    # Remove leading
+    tweet = re.sub(r'^(\#\w+\s?)+', '', tweet, flags=re.MULTILINE)
+
+    # Remove trailing
+    tweet = re.sub(r'(\#\w+\s?)+$', '', tweet, flags=re.MULTILINE)
+
+    return tweet
+
+def remove_whitespace(post):
+    return re.sub(r'\s+', ' ', post, flags=re.MULTILINE)
+
 def preprocess_tweets(tweets):
     return [preprocess_tweet(tweet) for tweet in tweets]
 
 def preprocess_tweet(tweet):
+    tweet = tweet.strip()
     tweet = remove_urls(tweet)
     tweet = replace_usernames(tweet)
-    tweet = tweet.replace('\n', ' ')
+    tweet = remove_trailing_and_leading_hashtags(tweet)
+    tweet = remove_whitespace(tweet)
     
     return tweet
+
+def preprocess_reddits(posts):
+    return [preprocess_reddit(post) for post in posts]
+
+def preprocess_reddit(post):
+    post = post.strip()
+    post = remove_urls(post)
+    post = replace_usernames(post)
+    post = remove_whitespace(post)
+
+    return post
